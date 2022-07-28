50c536da89a796b9a1436cc291cae8ab19e2a644
nvhoan295 <nvhoan295@gmail.com>
2022-07-28 13:59:30 +0700

    Create Assignment2.sql

---
+++ SQL/Assignment2.sql
@@ -0,0 +1,10 @@
+drop database if exists Assignment2;
+create database Assignment2;
+use Assignment2;
+create table Trainee (
+TraineeID		int primary key auto_increment,
+Full_Name		varchar(50),
+Birth_Date		date
+		
+
+);
