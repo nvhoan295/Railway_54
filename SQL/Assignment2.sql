f922f10b5ce2a35db2589f7d2c74fa9163858b20
nvhoan295 <nvhoan295@gmail.com>
2022-07-28 14:10:48 +0700

    Update Assignment2.sql

--- SQL/Assignment2.sql
+++ SQL/Assignment2.sql
@@ -2,9 +2,13 @@ drop database if exists Assignment2;
 create database Assignment2;
 use Assignment2;
 create table Trainee (
-TraineeID		int primary key auto_increment,
-Full_Name		varchar(50),
-Birth_Date		date
-		
-
+TraineeID			int primary key auto_increment,
+Full_Name			varchar(50),
+Birth_Date			date,
+Gender				int,
+ET_IQ				tinyint,
+ET_Gmath			tinyint,
+ET_English			tinyint,
+Training_Class		varchar(50),
+Evaluation_Notes	text
 );
