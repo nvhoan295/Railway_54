e77eba0c6660fdbc1f691a5ea8462371580b78bf
nvhoan295 <nvhoan295@gmail.com>
2022-07-29 21:12:51 +0700

    Update Assignment2.sql

--- SQL/Lesson1/Assignment2.sql
+++ SQL/Lesson1/Assignment2.sql
@@ -2,23 +2,28 @@ drop database if exists Assignment2;
 create database Assignment2;
 use Assignment2;
 create table Trainee (
-TraineeID			int primary key auto_increment,
+TraineeID			int AUTO_INCREMENT primary key,
 Full_Name			varchar(50),
 Birth_Date			date,
 `Gender`			enum("Male", "Female", "unknown"),
-ET_IQ				tinyint,
-ET_Gmath			tinyint,
-ET_English			tinyint,
+ET_IQ				int,
+check (ET_IQ >0 and ET_IQ<20),
+ET_Gmath			INT,
+check (ET_Gmath >0 and ET_Gmath<20),
+ET_English			INT,
+check (ET_English > 0 and ET_English< 50),
 Training_Class		varchar(50),
-Evaluation_Notes	text,
-VTI_Account			int not null unique
+Evaluation_Notes	text
 );
+
+ALTER TABLE Trainee
+ADD VTI_ACCOUNT INT not null UNIQUE AFTER TraineeID;
 -- Exercise 2:
 create table DataTypes (
-ID					int primary key auto_increment,
+ID					mediumint AUTO_INCREMENT primary key,
 `Name`				varchar(50),
-`Code`				varchar(255),
-ModifiedDate		date
+`Code`				char(5),
+ModifiedDate		DATETIME
 );
 
 -- Exercise 3:
@@ -26,6 +31,6 @@ create table  DataTypes2 (
 ID					int primary key auto_increment,
 `Name`				varchar(50),
 BirthDate			date,
-`Gender`			int,
-IsDeletedFlag		bool
+`Gender`			enum('Male','Female','Unknown'),
+IsDeletedFlag		ENUM('0','1')
 );
