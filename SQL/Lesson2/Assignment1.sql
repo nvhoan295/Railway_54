5f33024ebab2ae66cdec48f93b7b1de1843ea7aa
nvhoan295 <nvhoan295@gmail.com>
2022-08-01 10:02:26 +0700

    UPDATE

--- SQL/Lesson2/Assignment1.sql
+++ SQL/Lesson2/Assignment1.sql
@@ -3,19 +3,19 @@ CREATE DATABASE Testing_System_Assignment_1;
 USE Testing_System_Assignment_1;
 -- BANG 1
 CREATE TABLE Department (
-DepartmentID			INT PRIMARY KEY AUTO_INCREMENT,
+DepartmentID			INT AUTO_INCREMENT PRIMARY KEY,
 DepartmentName			VARCHAR(50)
 );
 
 -- BANG 2
 CREATE TABLE `Position` (
-PositionID				INT PRIMARY KEY AUTO_INCREMENT,
+PositionID				INT AUTO_INCREMENT PRIMARY KEY,
 PositionName			enum('Dev','Test','Scrum Master','PM')
 );
 
 -- BANG 3
 CREATE TABLE `Account` (
-AccountID				INT PRIMARY KEY AUTO_INCREMENT,
+AccountID				INT AUTO_INCREMENT PRIMARY KEY,
 Email					VARCHAR(50),
 Username				VARCHAR(50) UNIQUE,
 FullName				VARCHAR(50),
@@ -28,7 +28,7 @@ FOREIGN KEY (PositionID) REFERENCES `Position`(PositionID)
 
 -- BANG 4
 CREATE TABLE `Group` (
-GroupID					INT PRIMARY KEY,
+GroupID					INT AUTO_INCREMENT PRIMARY KEY,
 GroupName				VARCHAR(50),
 CreatorID				INT,
 CreateDate				DATE,
@@ -46,19 +46,19 @@ FOREIGN KEY (AccountID) REFERENCES `Account`(AccountID)
 
 -- BANG 6
 CREATE TABLE TypeQuestion (
-TypeID					INT PRIMARY KEY,
+TypeID					INT AUTO_INCREMENT PRIMARY KEY,
 TypeName				ENUM('Essay', 'Multiple-Choice')
 );
 
 -- BANG 7
 CREATE TABLE CategoryQuestion (
-CategoryID				INT PRIMARY KEY,
-CategoryName			VARCHAR(50)
+CategoryID				INT AUTO_INCREMENT PRIMARY KEY,
+CategoryName			ENUM('Java', '.NET', 'SQL', 'Postman', 'Ruby')
 );
 
 -- BANG 8
 CREATE TABLE Question (
-QuestionID				INT PRIMARY KEY,
+QuestionID				INT AUTO_INCREMENT PRIMARY KEY,
 Content					VARCHAR(255),
 CategoryID				INT,
 TypeID					INT,
@@ -71,7 +71,7 @@ FOREIGN KEY (CreatorID) REFERENCES `Account`(AccountID)
 
 -- BANG 9
 CREATE TABLE Answer (
-AnswerID				INT PRIMARY KEY,
+AnswerID				INT AUTO_INCREMENT PRIMARY KEY,
 Content					VARCHAR(255),
 QuestionID				INT,
 isCorrect				ENUM('ĐÚNG', 'SAI'),
@@ -80,7 +80,7 @@ FOREIGN KEY (QuestionID) REFERENCES Question(QuestionID)
 
 -- BANG 10
 CREATE TABLE Exam (
-ExamID					INT PRIMARY KEY,
+ExamID					INT AUTO_INCREMENT PRIMARY KEY,
 `Code`					VARCHAR(50),
 Title					VARCHAR(50),
 CategoryID				INT,
@@ -114,3 +114,46 @@ insert into `Account` (AccountID, Email, Username, FullName, DepartmentID, Posit
 insert into `Account` (AccountID, Email, Username, FullName, DepartmentID, PositionID, CreateDate) values (3, 'lcoste2@4shared.com', 'lcoste2', 'Lesya Coste', 2, 3, '2022-04-15');
 insert into `Account` (AccountID, Email, Username, FullName, DepartmentID, PositionID, CreateDate) values (4, 'crossant3@columbia.edu', 'crossant3', 'Carlo Rossant', 1, 1, '2022-01-15');
 insert into `Account` (AccountID, Email, Username, FullName, DepartmentID, PositionID, CreateDate) values (5, 'porro4@shinystat.com', 'porro4', 'Padriac Orro', 3, 4, '2021-09-12');
+
+insert into `Group` (GroupName, CreatorID, CreateDate) values ( 'Nhom1', 1 , '2022-04-23'),
+															('Nhom 2', 3, '2022-01-12'),
+                                                            ('Nhom 3', 2, '2021-03-25'),
+                                                            ('Nhom 4', 4, '2020-11-20');
+                                                            
+insert into GroupAccount (GroupID, AccountID, JoinDate) values (1 , 4, '2022-01-23'),
+																(2, 5, '2011-04-26'),
+                                                                (3 ,4, '2020-08-30'),
+                                                                (4, 2, '2022-09-25');
+                                                                
+insert into TypeQuestion ( TypeName) values ('Essay'),
+										('Multiple-Choice');
+                                        
+insert into CategoryQuestion(CategoryName) values ('Java'),
+												('.NET'),
+                                                ('SQL'),
+                                                ('Postman'),
+                                                ('Ruby');
+                                                            
+insert into Question (Content, CategoryID, TypeID, CreatorID, CreateDate) values ('Java là gì', 1 , 1, 2, '2020-02-18'),
+																				('.Net là gì',2, 2, 4, '2021-04-20'),
+                                                                                ('Postman là gì',3, 1, 5, '2020-04-10'),
+                                                                                ('.Ruby là gì',4, 2, 3, '2022-04-18');
+
+insert into Answer (Content, QuestionID, isCorrect) values ('Java là Ngôn ngữ lập trình bậc cao', 1, 'Đúng'),
+															('.Net là .....', 2, 'Sai'),
+                                                            ('Postman là .....', 3, 'Đúng'),
+                                                            ('Ryby là .....', 4, 'Sai');
+                                                            
+insert into Exam (`Code`, Title, CategoryID, Duration, CreatorID, CreateDate) values ( 'A1', 'Tự Luận', 1, '1:00:00', 1, '2022-03-15'),
+																					( 'B1', 'Vấn Đáp', 2, '1:00:00', 1, '2022-03-15'),
+                                                                                    ( 'C1', 'Code trên máy', 3, '1:00:00', 1, '2022-03-15');
+                                                                                    
+insert into ExamQuestion values (1, 1),
+								(2, 2),
+                                (3, 3);
+
+                                                            
+                                                            
+
+																					
+                                                            
