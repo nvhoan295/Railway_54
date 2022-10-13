7ad424a0cfd35d36298b2dce73919ea4c40c599c
nvhoan295 <nvhoan295@gmail.com>
2022-10-13 19:48:50 +0700

    Update AccountController.java

--- JAVA/Bai11/src/main/java/com/vti/backend/presentationlayer/AccountController.java
+++ JAVA/Bai11/src/main/java/com/vti/backend/presentationlayer/AccountController.java
@@ -201,7 +201,7 @@ public class AccountController {
 	 * @param emailAddress
 	 * @return
 	 */
-	public static boolean checkEmail(String emailAddress) {
+	public boolean checkEmail(String emailAddress) {
 		 String  regexPattern = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@" 
 			        + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
 		  Pattern pattern = Pattern.compile(regexPattern);  
